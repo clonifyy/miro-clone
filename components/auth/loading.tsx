602068291d0a30beba5c6354e2425616f35fc15e
Vien Huynh <vien200203@gmail.com>
2024-04-02 20:10:47 +0700

    feat(auth): setup auth with clerk and convex

---
+++ components/auth/loading.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export const Loading = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Loading...</h1>
+    </div>
+  );
+};
---
+++ convex/auth.config.js
@@ -0,0 +1,10 @@
+export default {
+    providers: [
+        {
+            domain: "https://closing-wahoo-9.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+
+
