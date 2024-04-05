d47d27b6f4dc837c8ad7c53713615b04c591c35f
Vien Huynh <vien200203@gmail.com>
2024-04-05 22:57:10 +0700

    chore: enhance SEO and loading

--- components/auth/loading.tsx
+++ components/auth/loading.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export const Loading = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Loading...</h1>
+      <Image src="/logo.svg" width={80} height={80} alt="Loading..." />
     </div>
   );
 };
